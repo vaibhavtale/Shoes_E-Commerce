7675c25508aee8ce3e67e1df75d994b7ab2fda25
vaibhav <vaibhavtale20@gmail.com>
2023-07-07 19:06:59 +0530

    Done with the IntroPage

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,44 +8,68 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          children: [
-            //  logo
-            Padding(
-              padding: const EdgeInsets.only(top: 150, left: 30),
-              child: Image.asset(
-                "images/logo.png",
-                height: 220,
-                width: 300,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //  logo
+              Padding(
+                padding: const EdgeInsets.only(top: 150, left: 30),
+                child: Image.asset(
+                  "images/logo.png",
+                  height: 220,
+                  width: 300,
+                ),
               ),
-            ),
 
-            SizedBox(
-              height: 20,
-            ),
-
-            // Text
-            Center(
-                child: Text(
-              "Just do it",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 55,
-                  fontStyle: FontStyle.italic),
-            )),
-
-            SizedBox(
-              height: 30,
-            ),
-
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "Consistant Practice and Regular smart work on Improving the tactics of Sports makes The Olympiad.",
-                style: TextStyle(fontSize: 16, color: Colors.grey),
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+
+              // Text
+              Text(
+                "Just do it",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 55,
+                    fontStyle: FontStyle.italic),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              Text(
+                "Brand new sneaker's and kicks made with premium quality.",
+                style: TextStyle(fontSize: 16, color: Colors.grey,),
+                textAlign: TextAlign.center,
+              ),
+
+              SizedBox(height: 50,),
+
+              GestureDetector(
+                onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),),),
+                child: Container(
+                  padding: EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
